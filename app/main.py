91701811e772b3447da78d668da9d6184345bfd6
Yuuki Shimizu <yuuki@maxio.jp>
2021-09-22 17:30:00 +0900

    リファクタリング

--- app/main.py
+++ app/main.py
@@ -7,7 +7,6 @@ import json
 import os
 
 target_url = "https://www.pref.yamanashi.jp/taiki-sui/documents/h3012011.pdf"
-file_name = "h3012011.pdf"
 
 root_path = os.getenv("ROOT_PATH", "")
 app = FastAPI(
@@ -17,36 +16,21 @@ app = FastAPI(
 
 @app.get("/")
 def read_root():
-    download_file_if_needed(target_url)
-    data = get_data(file_name)
+    data = get_data(target_url)
     json_data = data.to_json(orient = 'records')
     return json.loads(json_data)
 
 @app.get("/area/{area}")
 def read_item(area: str):
-    download_file_if_needed(target_url)
-    data = get_data(file_name)
+    data = get_data(target_url)
     df_mask = data['市町村名'] == area
     data = data[df_mask]
     json_data = data.to_json(orient = 'records')
     return json.loads(json_data)
 
-def check_columns(df, previous_df):
-    """前ページと現ページのデータフレーム比較"""
-    diff1 = set(df.keys()) - set(previous_df.keys())
-    diff2 = set(previous_df.keys()) - set(df.keys())
-    return (len(diff1) == 0 and len(diff2) == 0)
+def get_data(url):
+    path = download_file_if_needed(url)
 
-def download_file_if_needed(url):
-    """ローカルにデータファイルがない場合は、データファイルをダウンロードする"""
-    if (os.path.exists(file_name)):
-        return
-    
-    data = requests.get(url).content
-    with open(file_name ,mode='wb') as f:
-        f.write(data)
-
-def get_data(path):
     previous_df = pd.DataFrame()
 
     dfs = tabula.read_pdf(path, lattice=True, pages = 'all')
@@ -58,3 +42,20 @@ def get_data(path):
         previous_df = df
     
     return previous_df
+
+def check_columns(df, previous_df):
+    """前ページと現ページのデータフレーム比較"""
+    diff1 = set(df.keys()) - set(previous_df.keys())
+    diff2 = set(previous_df.keys()) - set(df.keys())
+    return (len(diff1) == 0 and len(diff2) == 0)
+
+def download_file_if_needed(url):
+    """ローカルにデータファイルがない場合は、データファイルをダウンロードする"""
+    dir = os.path.dirname(__file__)
+    file_path = dir + "/data.pdf"
+    if not os.path.exists(file_path):
+        data = requests.get(url).content
+        with open(file_path ,mode='wb') as f:
+            f.write(data)
+
+    return file_path
